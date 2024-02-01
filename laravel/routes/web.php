e1db30f411d0d4b760c6e690d74cd0304f98cca5
= <jovanvukovic2001@gmail.com>
2024-02-01 16:19:36 +0100

    prepravke web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -25,24 +25,4 @@ Route::get('/greeting', function () {
     return 'Hello World';
 });
 
-Route::get('/forgot-password', function () {
-    return view('auth.forgot-password');
-})->middleware('guest')->name('password.request');
-
-Route::post('/forgot-password', function (Request $request) {
-    $request->validate(['email' => 'required|email']);
- 
-    $status = Password::sendResetLink(
-        $request->only('email')
-    );
- 
-    return $status === Password::RESET_LINK_SENT
-                ? back()->with(['status' => __($status)])
-                : back()->withErrors(['email' => __($status)]);
-})->middleware('guest')->name('password.email');
-
-Route::get('/reset-password/{token}', function (string $token) {
-    return view('auth.reset-password', ['token' => $token]);
-})->middleware('guest')->name('password.reset');
-
 require __DIR__.'/auth.php';
